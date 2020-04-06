01aa9e931990516b3a1c69f35a59ae8f83cf0995
ranga.nadh@everleagues.com <ranga.nadh@everleagues.com>
2020-04-05 22:14:13 -0500

    Ranga: state selector refactor changes

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -1,4 +1,3 @@
-// import { LocalStorageService } from './../localStorage/local-storage.service';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,11 @@ import { OUService } from './services/ou.service';
 import { UserService } from './services/user.service';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthService } from './auth/auth.service';
-import { LocalStorageService } from './localStorage/local-storage.service';
 import { ApiService } from './api/api.service';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { EsignStateSelector } from '../esign/service/esign.state.selector';
 // import { CurrentOUComponent } from './ui/current-ou/current-ou.component';
 
 @NgModule({
@@ -19,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [],
   providers: [
     ApiService,
-    LocalStorageService,
+    EsignStateSelector,
     AuthService,
     OUService,
     UserService,
